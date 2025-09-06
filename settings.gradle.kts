6daa96d53228ac3df05c0a6744702d5f77abcd20
Himaya Senarathne <himayadasuni@gmail.com>
2025-09-06 19:01:24 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Added JitPack repository to find the calendar library
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "SmartShiksha"
+include(":app")
